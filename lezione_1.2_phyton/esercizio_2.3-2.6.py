0c9ef584ae5c9e2d92adb20df9839e3d1e816788
S1lvia2 <silviapesce28@gmail.com>
2025-02-17 12:14:25 +0100

    esercizi TIPI DI DATO

---
+++ lezione_1.2_phyton/esercizio_2.3-2.6.py
@@ -0,0 +1,19 @@
+# # esercizio 2.3
+# name = "Silvia"
+# print (f"Hello {name} would you like to learn some Phyton today?")
+
+# # esercizio 2.4
+# name = "Silvia"
+# print (name.lower())
+# print (name.upper())
+# print (name.title())
+
+# # esercizio 2.5
+# quote = "la vita non è quella che si è vissuta, ma quella che si ricorda e come la si ricorda per raccontarla"
+# author = "Gabriel Marquez"
+# print(f"{author} once said, {quote}")
+
+# # esercizio 2.6
+# famous_person = "Gabriel Marquez"
+# quote ="la vita non è quella che si è vissuta, ma quella che si ricorda e come la si ricorda per raccontarla"
+# print(f"{famous_person} once said, {quote}")
