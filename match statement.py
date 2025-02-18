7f7fd7716ff58703a55c38ad957ad69196c82ced
S1lvia2 <silviapesce28@gmail.com>
2025-02-18 11:23:02 +0100

    match statement

---
+++ match statement.py
@@ -0,0 +1,18 @@
+# n: input = int(input("insert finishing position:"))
+
+# match n:
+# #n= 1
+#       case 1:                            
+#           print (f"{n}st!")
+
+# # n=2
+#       case 2:                             
+#          print (f"{n}nd!")
+# # n=3
+#       case 3:                             
+#          print (f"{n}rd!")
+
+#       case _:                               #default case 
+#         print(f"{n}th!")
+
+
