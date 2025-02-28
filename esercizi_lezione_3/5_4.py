b091d54c2e55bf2a28b10ded9e25c3273b1be200
S1lvia2 <silviapesce28@gmail.com>
2025-02-28 19:00:28 +0100

    esercizi aggiuntivi cicli

---
+++ esercizi_lezione_3/5_4.py
@@ -0,0 +1,7 @@
+alien_colors : str = "red"
+
+if alien_colors == "green": 
+     print("hai guadagnato 5 punti")
+else: 
+     print( "hai guadagnato 10 punti")
+
---
+++ esercizi_lezione_3/5_5.py
@@ -0,0 +1,34 @@
+alien_colors : str = "green"
+
+if alien_colors == "green": 
+     print("hai guadagnato 5 punti")
+
+if alien_colors == "yellow": 
+     print("hai guadagnato 10 punti")
+
+if alien_colors == "red": 
+     print("hai guadagnato 15 punti")
+
+
+alien_colors : str = "yellow"
+
+if alien_colors == "green": 
+     print("hai guadagnato 5 punti")
+
+if alien_colors == "yellow": 
+     print("hai guadagnato 10 punti")
+
+if alien_colors == "red": 
+     print("hai guadagnato 15 punti")
+
+
+alien_colors : str = "red"
+
+if alien_colors == "green": 
+     print("hai guadagnato 5 punti")
+
+if alien_colors == "yellow": 
+     print("hai guadagnato 10 punti")
+
+if alien_colors == "red": 
+     print("hai guadagnato 15 punti")
